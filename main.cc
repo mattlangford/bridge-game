72cb001f28b5d939608ece37b855c5ca6092e934
Matt Langford <matthew.langford95@gmail.com>
2020-12-29 15:43:26 -0500

    Get main working again

--- main.cc
+++ main.cc
@@ -1,39 +1,65 @@
-#include <GLFW/glfw3.h>
 #include <iostream>
 
-void error_callback(int code, const char* description)
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+int main()
 {
-    std::cerr << "GLFW Error code: " << code << " message: " << description << std::endl;
-    throw std::runtime_error(description);
-}
+    // start GL context and O/S window using the GLFW helper library
+    if (!glfwInit()) {
+        return 1;
+    }
 
-int main(void)
-{
-    GLFWwindow* window;
+    const GLint WIDTH = 800, HEIGHT = 600;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    // Mac specific params
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
-    /* Make the window's context current */
+    int screenWidth, screenHeight;
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if (GLEW_OK != glewInit()) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return EXIT_FAILURE;
     }
 
+    // Define the viewport dimensions
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window)) {
+        // Check if any events have been activiated (key pressed,
+        //mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw OpenGL
+
+        glfwSwapBuffers(window);
+    }
+
+    // close GL context and any other GLFW resources
     glfwTerminate();
     return 0;
 }
