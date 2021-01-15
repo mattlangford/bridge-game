26a356a398fea8fa461573a1082740372ed2287f
Matt Langford <matthew.langford95@gmail.com>
2021-01-15 18:02:33 -0500

    Proper test runner

--- test.cc
+++ test.cc
@@ -1,5 +1,47 @@
 #include "mesh.hh"
 
-int main()
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+void test_mesh()
 {
+}
+
+int main(int argc, char* argv[])
+{
+    std::unordered_map<std::string, std::function<void(void)>> tests;
+    tests["mesh"] = &test_mesh;
+
+    if (argc == 2) {
+        const std::string name = argv[1];
+        auto it = tests.find(name);
+        if (it == std::end(tests)) {
+            std::cout << "No tests found named '" << name << "'!\n";
+            return 1;
+        }
+
+        std::cout << "Test: '" << it->first << "'\n";
+        try {
+            it->second();
+        } catch (const std::exception& ex) {
+            std::cout << "FAILED: '" << ex.what() << "'\n";
+            return 1;
+        }
+        std::cout << "PASSED\n";
+    } else {
+        std::cout << "Running all tests!\n";
+        for (const auto& [name, func] : tests) {
+            std::cout << "Test: '" << name << "'\n";
+            try {
+                func();
+            } catch (const std::exception& ex) {
+                std::cout << "FAILED: '" << ex.what() << "'\n";
+                continue;
+            }
+            std::cout << "PASSED\n";
+        }
+    }
+
+    return 0;
 }
