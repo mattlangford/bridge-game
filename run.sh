50c377074bc2ef7bceb9307bc7307ee6b28ba270
Matt Langford <matthew.langford95@gmail.com>
2021-01-17 17:24:12 -0500

    Messages while building

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-clang++ -o main -std=c++2a main.cc -O3 -ferror-limit=1 -lglfw -lglew -framework OpenGL -DGL_SILENCE_DEPRECATION && ./main $@
+echo "Building..." && clang++ -o main -std=c++2a main.cc -O3 -ferror-limit=1 -lglfw -lglew -framework OpenGL -DGL_SILENCE_DEPRECATION && echo "Done Building. Running..." && ./main $@
