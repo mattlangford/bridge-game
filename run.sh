250a62af24e88ef134b7d13a09e83e7b28950579
Matt Langford <matthew.langford95@gmail.com>
2021-01-17 18:00:02 -0500

    Update bash scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-echo "Building..." && clang++ -o main -std=c++2a main.cc -O3 -ferror-limit=1 -lglfw -lglew -framework OpenGL -DGL_SILENCE_DEPRECATION && echo "Done Building. Running..." && ./main $@
+function build(){
+    echo "Building $1.cc..."
+    clang++ -o $1 -std=c++2a $1.cc -O3 -ferror-limit=1 -I/usr/local/include/eigen3/ -lglfw -lglew -framework OpenGL -DGL_SILENCE_DEPRECATION
+}
+
+if [ "$#" -eq 0 ]; then
+    build main && echo "Done Building. Running..." && ./main $@
+elif [ "$#" -eq 1 ] && [ "$1" == "test" ]; then
+    shift # Remove the first arg (which is "test")
+    build test && echo "Done Building. Testing..." && ./test $@
+fi
--- test.sh
+++
@@ -1,1 +0,0 @@
-clang++ -o test -std=c++2a test.cc -O3 -ferror-limit=1 -lglfw -lglew -framework OpenGL -DGL_SILENCE_DEPRECATION && ./test $@
