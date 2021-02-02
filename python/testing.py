9ac0153fadb70e8a19009f574fc9cf63d7ef3906
Matt Langford <matthew.langford95@gmail.com>
2021-02-02 11:42:04 -0500

    Add energy calculations

--- python/testing.py
+++ python/testing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import sys
 
 np.set_printoptions(suppress=True, edgeitems=10, linewidth=100000)
 
@@ -16,14 +17,13 @@ D = np.array([
 ])
 D *= E / (1.0 - v * v)
 
-fps = 100.
+fps = 250.
 dt = 1 / fps
 dt2 = dt * dt
 
 coords = np.array([
-    4, 0,
-    0, 0,
     0, 1,
+    0, 0,
     1, 1,
     1, 0,
     2, 1,
@@ -31,21 +31,22 @@ coords = np.array([
     3, 1,
     3, 0,
     4, 1,
+    4, 0,
 ], dtype=np.float64)
 
 C = c * np.eye(len(coords))
 
 fixed = np.array([
-    0, 0,
     1, 1,
     1, 1,
     0, 0,
     0, 0,
-    0, 0,
-    0, 0,
-    0, 0,
-    0, 0,
-    0, 0,
+    1, 1,
+    1, 1,
+    1, 1,
+    1, 1,
+    1, 1,
+    1, 1,
 ])
 assert len(fixed) == len(coords)
 
@@ -56,16 +57,13 @@ def get_triangle(p0, p1, p2):
         2 * p2, 2 * p2 + 1
     ]
 
-triangles = [
-    get_triangle(1, 2, 3),
-    get_triangle(1, 3, 4),
-    get_triangle(3, 4, 5),
-    get_triangle(4, 5, 6),
-    get_triangle(5, 6, 7),
-    get_triangle(6, 7, 8),
-    get_triangle(7, 8, 9),
-    get_triangle(8, 9, 0),
-]
+def get_triangles(from_i, to_i):
+    l = []
+    for i in range(from_i, to_i):
+        l.append(get_triangle(i, i + 1, i + 2))
+    return l
+
+triangles = get_triangles(0, 2)
 print (triangles)
 u = np.zeros_like(coords)
 u_dot = np.zeros_like(u)
@@ -73,13 +71,12 @@ u_dot_dot = np.zeros_like(u)
 u_dot_dot[1::2] = -9.8
 
 def M():
-    triangle_volume = 5 * 5 * 5 / 2
+    triangle_volume = 1 * 1 * 1 / 2
     M = np.zeros((len(coords), len(coords)))
     for triangle in triangles:
         mass = triangle_volume * m / 6
         for pt in triangle:
-            if not fixed[pt]:
-                M[pt, pt] += mass
+            M[pt, pt] += mass
     return M
 
 def area(triangle):
@@ -119,13 +116,9 @@ def b(triangle):
     return b / det_j
 
 def k(triangle):
-    thickness = 20
+    thickness = 1
     b_ = b(triangle)
     k = thickness * area(triangle) * b_.T.dot(D).dot(b_)
-    # for i, is_fixed in enumerate(fixed[triangle]):
-    #     if is_fixed:
-    #         k[i, :] = 0
-    #         k[:, i] = 0
     return k
 
 def K():
@@ -176,6 +169,13 @@ def generate_nonfixed_vector(full_vector):
     for to_i, from_i in enumerate(coords_map):
         result[to_i] = full_vector[from_i]
     return result
+
+def compute_energy(u, u_dot):
+    kinetic_energy = 0.5 * u_dot.transpose().dot(M).dot(u_dot)
+    strain_potential_energy = 0.5 * u.transpose().dot(K).dot(u)
+    gravitational_potential_energy = -9.8 * np.sum(M.dot(-u)[1::2])
+
+    return kinetic_energy + strain_potential_energy + gravitational_potential_energy
 
 def update(iteration):
     global u, u_dot, u_dot_dot
@@ -229,7 +229,7 @@ def draw_triangles(u):
         x1, y1, x2, y2, x3, y3 = points[triangle]
 
         stresses = D.dot(b(triangle)).dot(u[triangle])
-        stress = np.linalg.norm(stresses)
+        stress = np.linalg.norm(stresses[:2])
 
         def color(c):
             return max(min(c, 1.0), 0.0)
@@ -255,22 +255,32 @@ def draw(i, u):
     else:
         plt.savefig(f"/tmp/{i}.png")
 
-draw("00_init", u)
-gravity = np.zeros_like(u)
-for i in range(len(gravity)):
-    if i % 2 != 0:
-        gravity[i] = -9.8 * M[i, i]
-ref_u = generate_full_vector(np.linalg.inv(generate_nonfixed_matrix(K)).dot(generate_nonfixed_vector(gravity)), np.zeros_like(u))
-draw("00_steady", ref_u)
 
-i = 1
-while True:
-    print (f"Generating frame {i}")
-    update(i)
+if __name__ == "__main__":
+    draw("00_init", u)
+    print ("Initial energy:", compute_energy(u, np.zeros_like(u)))
 
-    print (f"u: {u}")
+    gravity = np.zeros_like(u)
+    for i in range(len(gravity)):
+        if i % 2 != 0:
+            gravity[i] = -9.8 * M[i, i]
 
-    if i % int(1 / 30 * fps) == 0:
-        draw(i, u)
+    ref_u = generate_full_vector(np.linalg.inv(generate_nonfixed_matrix(K)).dot(generate_nonfixed_vector(gravity)), np.zeros_like(u))
+    draw("00_steady", ref_u)
+    print ("Steady energy:", compute_energy(ref_u, np.zeros_like(u)))
 
-    i += 1
+    max_i = 5000
+    if len(sys.argv) == 2:
+        max_i = int(sys.argv[1])
+
+    for i in range(max_i):
+        print (f"Generating frame {i}")
+        update(i)
+        print (f"Total Energy at {i}:", compute_energy(u, u_dot))
+
+        print (f"u: {u}")
+
+        if i % int(1 / 30 * fps) == 0:
+            draw(i, u)
+
+        i += 1
