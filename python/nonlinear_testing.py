6765184ad926b50c16c6817c4ec8eac4e73d2c29
Matt Langford <matthew.langford95@gmail.com>
2021-02-09 22:46:06 -0500

    Meh this doesn't work

--- python/nonlinear_testing.py
+++ python/nonlinear_testing.py
@@ -17,7 +17,13 @@ D = np.array([
 ])
 D *= E / (1.0 - v * v)
 
-fps = 250.
+D = np.array([
+    [2 * v + E, E, 0.0],
+    [E, 2 * v + E, 0.0 ],
+    [0.0, 0.0, v]
+])
+
+fps = 2000.
 dt = 1 / fps
 dt2 = dt * dt
 
@@ -27,6 +33,10 @@ coords = np.array([
     0, 1,
     1, 0,
     1, 1,
+    2, 0,
+    2, 1,
+    3, 0,
+    3, 1,
     # 1, 2,
     # 1, 3,
     # 2, 3,
@@ -48,6 +58,10 @@ C = c * np.eye(len(coords))
 fixed = np.array([
     1, 1,
     1, 1,
+    0, 0,
+    0, 0,
+    0, 0,
+    0, 0,
     0, 0,
     0, 0,
     # 0, 0,
@@ -82,11 +96,11 @@ def get_triangles(from_i, to_i):
 
 triangles = [
     get_triangle(0, 1, 2),
-    #get_triangle(3, 2, 1),
-    # get_triangle(1, 2, 4),
-    # get_triangle(3, 4, 9),
-    # get_triangle(4, 5, 8),
-    # get_triangle(5, 6, 7),
+    get_triangle(3, 2, 1),
+    get_triangle(2, 3, 4),
+    get_triangle(5, 4, 3),
+    get_triangle(4, 5, 6),
+    get_triangle(7, 6, 5),
     # get_triangle(5, 8, 7),
     # get_triangle(4, 8, 9),
     # get_triangle(3, 9, 10),
@@ -107,7 +121,112 @@ triangles = [
 u = np.zeros_like(coords)
 u_dot = np.zeros_like(u)
 u_dot_dot = np.zeros_like(u)
-u_dot_dot[1::2] = -9.8
+
+def compute_M():
+    triangle_volume = 1 * 1 * 1 / 2
+    M = np.zeros((len(coords), len(coords)))
+    for triangle in triangles:
+        mass = triangle_volume * m / 6
+        for pt in triangle:
+            M[pt, pt] += mass
+    return M
+M = compute_M()
+
+def compute_F(triangle):
+    sign = 1 if triangles.index(triangle) % 2 == 0 else -1
+    _u, _v = u[triangle][::2], u[triangle][1::2]
+
+    u10 = _u[1] - _u[0]
+    u20 = _u[2] - _u[0]
+    v10 = _v[1] - _v[0]
+    v20 = _v[2] - _v[0]
+    F = np.array([
+        [u10, u20],
+        [v10, v20]])
+    F /= d
+    F += np.eye(2)
+    F *= sign
+    return F
+
+def compute_F(triangle):
+    sign = 1 if triangles.index(triangle) % 2 == 0 else -1
+    c = coords[triangle]
+    points = c + u[triangle]
+
+    x = np.array([
+        [c[0], c[1]],
+        [c[2], c[3]],
+        [c[4], c[5]],
+    ])
+    y = np.array([
+        [points[0], points[1]],
+        [points[2], points[3]],
+        [points[4], points[5]],
+    ])
+
+    Y = np.array([y[1] - y[0], y[2] - y[0]])
+    X = np.array([x[1] - x[0], x[2] - x[0]])
+    F = Y.dot(np.linalg.inv(X))
+    return F
+
+def compute_normals(triangle):
+    sign = 1 if triangles.index(triangle) % 2 == 0 else -1
+    _u, _v = u[triangle][::2], u[triangle][1::2]
+
+    _d = sign * d
+    p10 = np.array([
+        -(_d + _v[1] - _v[0]),
+               _u[1] - _u[0]
+    ])
+    p20 = np.array([
+               _v[2] - _v[0],
+        -(_d + _u[2] - _u[0])
+    ])
+    p12 = np.array([
+         _d + _v[1] - _v[2],
+      -(-_d + _u[1] - _u[2]),
+    ])
+
+    return p10, p20, p12
+
+def compute_forces(triangle):
+    sign = 1 if triangles.index(triangle) % 2 == 0 else -1
+    _u, _v = u[triangle][::2], u[triangle][1::2]
+
+    F = compute_F(triangle)
+    strain = 0.5 * (F.T.dot(F) - np.eye(2))
+    stress = D.dot([strain[0, 0], strain[1, 1], strain[0, 1]])
+    S = np.array([
+        [stress[0], stress[2]],
+        [stress[2], stress[1]],
+    ])
+    p10, p20, p12 = compute_normals(triangle)
+
+    f0 = -0.5 * F.dot(S).dot(p20 + p10)
+    f1 = -0.5 * F.dot(S).dot(p10 + p12)
+    f2 = -0.5 * F.dot(S).dot(p12 + p20)
+    return f0, f1, f2
+
+def update():
+    global u, u_dot, u_dot_dot
+    forces = -9.8 * np.diag(M)
+
+    for triangle in triangles:
+        f0, f1, f2 = compute_forces(triangle)
+        forces[triangle[0]] += f0[0]
+        forces[triangle[1]] += f0[1]
+        forces[triangle[2]] += f1[0]
+        forces[triangle[3]] += f1[1]
+        forces[triangle[4]] += f2[0]
+        forces[triangle[5]] += f2[1]
+
+    forces[fixed == 1] = 0
+    #print (forces)
+
+    u_dot_dot = forces / np.diag(M)
+    u = u + dt * u_dot + 0.5 * dt2 * u_dot_dot
+    u_dot = u_dot + dt * u_dot_dot
+    print (u)
 
 def draw_triangles(u):
     points = coords + u
@@ -116,51 +235,38 @@ def draw_triangles(u):
         x0, y0, x1, y1, x2, y2 = points[triangle]
         plt.scatter((x0, x1, x2), (y0, y1, y2))
 
+        f0, f1, f2 = compute_forces(triangle)
+        f0 *= 1E-6
+        f1 *= 1E-6
+        f2 *= 1E-6
+
         _u = u[triangle][::2]
         _v = u[triangle][1::2]
 
         sign = 1 if i % 2 == 0 else -1
-        _d = sign * d
-        p10 = np.array([
-            -(_d + _v[1] - _v[0]),
-                   _u[1] - _u[0]
-        ])
-        p20 = np.array([
-                   _v[2] - _v[0],
-            -(_d + _u[2] - _u[0])
-        ])
-        p12 = np.array([
-            d + _v[1] - _v[2],
-          -(-d + _u[1] - _u[2]),
-        ])
-        print (_u[1] - _u[2], _v[1] - _v[2])
-
-        print (_u, _v)
-        print (p10, p20, p12)
+        p10, p20, p12 = compute_normals(triangle)
 
         plt.plot(
             [x0, x1, x1, x2, x2, x0],
             [y0, y1, y1, y2, y2, y0])
 
-        plt.plot(
-            [(x1 + x0) / 2.0, (x1 + x0) / 2.0 + p10[0]],
-            [(y1 + y0) / 2.0, (y1 + y0) / 2.0 + p10[1]])
-        plt.plot(
-            [(x2 + x0) / 2.0, (x2 + x0) / 2.0 + p20[0]],
-            [(y2 + y0) / 2.0, (y2 + y0) / 2.0 + p20[1]])
-        plt.plot(
-            [(x2 + x1) / 2.0, (x2 + x1) / 2.0 + p12[0]],
-            [(y2 + y1) / 2.0, (y2 + y1) / 2.0 + p12[1]])
+        plt.plot([x0, x0 + f0[0]], [y0, y0 + f0[1]])
+        plt.plot([x1, x1 + f1[0]], [y1, y1 + f1[1]])
+        plt.plot([x2, x2 + f2[0]], [y2, y2 + f2[1]])
+
+        # plt.plot([(x1 + x0) / 2.0, (x1 + x0) / 2.0 + p10[0]], [(y1 + y0) / 2.0, (y1 + y0) / 2.0 + p10[1]])
+        # plt.plot([(x2 + x0) / 2.0, (x2 + x0) / 2.0 + p20[0]], [(y2 + y0) / 2.0, (y2 + y0) / 2.0 + p20[1]])
+        # plt.plot([(x2 + x1) / 2.0, (x2 + x1) / 2.0 + p12[0]], [(y2 + y1) / 2.0, (y2 + y1) / 2.0 + p12[1]])
 
 
 def draw(i, u):
     plt.clf()
+    plt.gca().set_aspect('equal', adjustable='box')
+
     print (f"Saving frame {i}")
     plt.xlim(min(coords) - 2, max(coords) + 2)
     plt.ylim(min(coords) - 2, max(coords) + 2)
     draw_triangles(u)
-
-    plt.gca().set_aspect('equal', adjustable='box')
 
     if isinstance(i, int):
         plt.savefig(f"/tmp/{i:07}.png")
@@ -168,6 +274,17 @@ def draw(i, u):
         plt.savefig(f"/tmp/{i}.png")
 
 if __name__ == "__main__":
-    u[2] -= 0.5
-    u[4] += 3
     draw("00_test", u)
+
+    max_i = 5000
+    if len(sys.argv) == 2:
+        max_i = int(sys.argv[1])
+
+    for i in range(max_i):
+        print (f"Generating frame {i}")
+        update()
+
+        if i % int(1 / 60 * fps) == 0:
+            draw(i, u)
+
+        i += 1
